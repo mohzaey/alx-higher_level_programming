d439c55bc8fec0d6e7cedd439b621f56bbfdf839
mohzaey <moh.balogun@gmail.com>
2022-07-19 08:57:55 -0700

    tryal

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
+"""Create a Class Square with size, method of area and getters & setters"""
 
 
-def print_square(size):
-    """Print a square with the # character.
-    Args:
-        size (int): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+class Square:
+    """Class - Square"""
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    def __init__(self, size=0):
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Getter of the private attribute size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size private attribute"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
